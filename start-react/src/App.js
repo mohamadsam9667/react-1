b595e8a8b3d4310ab9997ac6eb7daf9df121922b
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-04-22 09:46:00 +0330

    using props in component
    استفاده از پراپس در کامپوننت

--- start-react/src/App.js
+++ start-react/src/App.js
@@ -3,29 +3,34 @@ import './App.css';
 
 function App() {
 
- 
-    
 
-const User = () => {
+  const GetName = (name) => {
+    return name;
+  } 
+    // //props
+    // const props={
+    //   name:"sarvin",
+    //   age:34,
+    //   email:ziba;
+    // }
+
+const User = (props) => {
   return(
     <div>
-      <h1>Sarvin</h1>
-      <h1>sam</h1>
-      <h2>sarvin</h2>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h2>{props.email}</h2>
     </div>
   )
 }
 
   return (
     <div>
-      <User></User>
+      <User name="sara" age={45} email="sam.ir" ></User>
       <User></User>
     </div>
   )
 
-  const GetName = () => {
-    return "sarvin";
-  }
 
 }
 

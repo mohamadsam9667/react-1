cefc02e9ed171d8f01b910d0e5e093b67380f24f
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-04-22 09:14:27 +0330

    component

--- start-react/src/App.js
+++ start-react/src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const user =
+    <div>
+      <h1>sam</h1>
+      <h2>sarvin</h2>
     </div>
-  );
+
+const User = () => {
+  return <h1>Sarvin</h1>
+}
+
+  return (
+    <div>
+      <User></User>
+    </div>
+  )
+
+  const GetName = () => {
+    return "sarvin";
+  }
+
 }
 
 export default App;

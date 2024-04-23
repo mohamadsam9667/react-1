bf351f2a4a99966123921f3e3d21de637e13cd0d
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-04-23 15:33:45 +0330

    remove props image
    به یادگیری اضافه کردن عکس در کامپوننت نرسیدم

--- start-react/src/App.js
+++ start-react/src/App.js
@@ -7,7 +7,6 @@ function App() {
     <div>
         <h1><em>{props.name}</em></h1>
         <h2>{props.age}</h2>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-NSEUHWmQsGxt4SfVM3f8VMW7vN8JsHnL-CnVII5E4A&s">{props.src}</img>
         <h3><em>{props.email}</em></h3>
 
     </div>
@@ -16,7 +15,7 @@ function App() {
   return(
     <div>
       <User name="sam" age={45} email="mohamadsam9667@gmail.com" ></User>
-      <User name="sara" age={30} email="reza2zt@gmail.com" img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-NSEUHWmQsGxt4SfVM3f8VMW7vN8JsHnL-CnVII5E4A&s">                      </User>
+      <User name="sara" age={30} email="reza2zt@gmail.com">                      </User>
       <User>                      </User>
 
 

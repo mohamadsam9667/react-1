d02904697a52359df77b0a9965e7cb3f02042243
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-04-23 15:24:16 +0330

    add in commponent image name age and/...

--- start-react/src/App.js
+++ start-react/src/App.js
@@ -2,9 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const User=(props)=>{
+    return(
+    <div>
+        <h1><em>{props.name}</em></h1>
+        <h2>{props.age}</h2>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-NSEUHWmQsGxt4SfVM3f8VMW7vN8JsHnL-CnVII5E4A&s">{props.src}</img>
+        <h3><em>{props.email}</em></h3>
+
+    </div>
+    )
+  }
+  return(
+    <div>
+      <User name="sam" age={45} email="mohamadsam9667@gmail.com" ></User>
+      <User name="sara" age={30} email="reza2zt@gmail.com" img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-NSEUHWmQsGxt4SfVM3f8VMW7vN8JsHnL-CnVII5E4A&s">                      </User>
+      <User>                      </User>
 
 
-  const GetName = (name) => {
+
+    </div>  
+    
+  
+  
+  )
+
+
+ 
+
+
+}
+
+export default App;
+/* const GetName = (name) => {
     return name;
   } 
     // //props
@@ -29,9 +59,4 @@ const User = (props) => {
       <User name="sara" age={45} email="sam.ir" ></User>
       <User></User>
     </div>
-  )
-
-
-}
-
-export default App;
+  )*/ 

6eab794f7d21f7ef488c65a0a97a45696aa2e3ca
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-05-05 00:13:37 +0330

    errror and error
    صبح دوباره شروع میکنم به اصلالح

--- start-react/src/Course.js
+++ start-react/src/Course.js
@@ -6,6 +6,9 @@ const Course=(props)=>{
     
         <h1  >{props.course.courseName}</h1>
         <button   onClick={() => props.deleteCourse(props.course.id)}>x</button>
+        
+     
+     
       </div>)
     }
     export default Course
